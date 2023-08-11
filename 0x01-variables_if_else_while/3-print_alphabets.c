9a46ec73400bf58c8976a5b349c0fa85f3c020f1
“Dino” <daringamaleldin@hotmail.com>
2023-08-11 13:27:19 -0700

    Alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,20 +2,16 @@
 /**
  *main - Entry point
  *
- *Return : Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
 char alphabet;
 char ALPHABET;
 for (alphabet = 'a'; alphabet <= 'z' ; alphabet++);
-{
 putchar(alphabet);
-}
 for (ALPHABET = 'A'; ALPHABET <= 'Z' ; ALPHABET++);
-{
 putchar(ALPHABET);
-}
 putchar('\n');
 return (0);
 }
