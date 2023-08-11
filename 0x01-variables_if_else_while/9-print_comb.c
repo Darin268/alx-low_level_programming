456170cedc69927409fedb3b741145b28496b831
“Dino” <daringamaleldin@hotmail.com>
2023-08-11 16:09:39 -0700

    combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num;
+for (num = 0; num <= 9; num++)
+{
+if (num <= 9 && num > 0)
+{
+putchar(',');
+putchar(' ');
+}
+putchar((num % 10) + '0');
+}
+putchar('$');
+return (0);
+}
