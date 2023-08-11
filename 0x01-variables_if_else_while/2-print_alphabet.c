21e1220ca372e63017fceb30db9493971dbd51ba
“Dino” <daringamaleldin@hotmail.com>
2023-08-11 12:36:19 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 char alphabet;
-for (alphabet = 'a'; alphabet <= 'z '; alphabet++)
-putchar(alphabet);
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+putchar(alphabet );
 return (0);
 }
