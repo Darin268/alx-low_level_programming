85e06de363b742a0db9b409e9d95ae8683d96d63
“Dino” <daringamaleldin@hotmail.com>
2023-08-11 12:31:46 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
-int main() 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 char alphabet;
-for (alphabet = 'a'; alphabet <= 'z'; ++alphabet)
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 putchar(alphabet);
 return (0);
 }
