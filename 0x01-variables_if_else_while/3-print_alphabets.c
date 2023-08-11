c66bbd715b97390ab22a91e585bfe7d9586ab5b0
“Dino” <daringamaleldin@hotmail.com>
2023-08-11 13:43:47 -0700

    ALPHABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,11 +8,11 @@ int main(void)
 {
 char alphabet;
 char ALPHABET;
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++);
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 putchar(alphabet);
 }
-for (ALPHABET = 'A'; ALPHABET <= 'Z'; ALPHABET++);
+for (ALPHABET = 'A'; ALPHABET <= 'Z'; ALPHABET++)
 {
 putchar(ALPHABET);
 }
