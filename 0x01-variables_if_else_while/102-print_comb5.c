2d2d57ae22c21c03e4c4ebce101126c9b756d094
“Dino” <daringamaleldin@hotmail.com>
2023-08-14 06:03:08 -0700

    print102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,16 +6,23 @@
  */
 int main(void)
 {
-	int num1, num2;
+	int num1, num2, num3, num4;
 
 	for (num1 = 0; num1 < 10; num1++)
 {
-	for (num2 = 0; num2 < 10; num2++)
+	for (num2 = 0; num2 < 9; num2++)
 	{
+		for (num3 = 0; num3 < 10; num3++)
+		{
+			for (num4 = 0; num4 < 10; num4++)
+			{
 		putchar((num1 % 10) + '0');
 		putchar((num2 % 10) + '0');
+		putchar(' ');
+		putchar((num3 % 10) + '0');
+		putchar((num4 % 10) + '0');
 
-		if (num1 == 9 && num2 == 9)
+		if (num1 == 9 && num2 == 9 && num3 == 9 && num == 9)
 			continue;
 		putchar(',');
 		putchar(' ');
