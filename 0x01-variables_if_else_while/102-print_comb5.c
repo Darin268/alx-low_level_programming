a829936f305ae91f1bd52c5934b5666ba6558bc9
“Dino” <daringamaleldin@hotmail.com>
2023-08-14 06:12:32 -0700

    print102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -22,7 +22,7 @@ int main(void)
 		putchar((num3 % 10) + '0');
 		putchar((num4 % 10) + '0');
 
-		if (num1 == 9 && num2 == 9 && num3 == 9 && num4 == 9)
+		if (num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9)
 			continue;
 		putchar(',');
 		putchar(' ');
