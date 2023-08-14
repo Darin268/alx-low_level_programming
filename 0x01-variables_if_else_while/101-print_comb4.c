b621f08172cc811b8bc986277be9152af354be23
“Dino” <daringamaleldin@hotmail.com>
2023-08-14 05:21:35 -0700

    print 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,25 +5,25 @@
  */
 int main(void)
 {
-        int num1, num2, num3;
+	int num1, num2, num3;
 
-        for (num1 = 0; num1 < 9; num1++)
-        {
-                for (num2 = num1 + 1; num2 < 10; num2++)
-                {
-			for (num3 = num2 + 1; num3 < 100; num3++)
+	for (num1 = 0; num1 < 8; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 9; num2++)
+		{
+			for (num3 = num2 + 1; num3 < 10; num3++)
 			{
-                        putchar((num1 % 10) + '0');
-                        putchar((num2 % 10) + '0');
-			putchar((num3 % 10) + '0');
+				putchar((num1 % 10) + '0');
+				putchar((num2 % 10) + '0');
+				putchar((num3 % 10) + '0');
 
-                        if (num1 == 7 && num2 == 8 && num3 == 9)
-                                continue;
-                        putchar(',');
-                        putchar(' ');
+				if (num1 == 7 && num2 == 8 && num3 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
 			}
-                }
-        }
-        putchar('\n');
-        return (0);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
