1d34a0abc22a9c8a60f1aed42037ab58b5624f7f
“Dino” <daringamaleldin@hotmail.com>
2023-08-15 06:50:00 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_alphabet(void)
 {
 	char c;
 
@@ -13,5 +13,5 @@ int main(void)
 		_putchar(c);
 	}
 	_putchar('\n');
-	return (0);
+	return (void);
 }
