80de69a6120627c522dc81209363732ce13d8f3f
“Dino” <daringamaleldin@hotmail.com>
2023-08-15 07:43:19 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
- * print_alphabet - Entry point
+ *print_alphabet  - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 void print_alphabet(void)
 {
-	char c = 'a';
+	char c;
 
-	while (c <= 'z')
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
-		c++;
+		
 	}
 		_putchar('\n');
 
