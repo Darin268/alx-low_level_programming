1e80930149fd185e3bd2f7a33322fdc634d8c48c
“Dino” <daringamaleldin@hotmail.com>
2023-08-15 09:11:07 -0700

    alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - Entry point
+ * @c: is the character to check
+ * Return: 1 if lowercase or  uppercase  and 0 if otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c >='A')
+	{
+		return (1);
+	}
+	else
+	{
+		return(0);
+	}
+}
