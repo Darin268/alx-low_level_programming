85abfdcd765ce4e8960bc8876fb75d077656336c
“Dino” <daringamaleldin@hotmail.com>
2023-08-15 11:36:31 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ *
+ * @x: is the number to check
+ * @y: is absolute value of x
+ *
+ * Return: 0 Always (Success)
+ */
+int _abs(int x, y)
+{
+	y = abs(x);
+	_putchar(y);
+	return (0);
+}
