6e3e0c1601c85b7d6da66b500c883ddf36952a28
“Dino” <daringamaleldin@hotmail.com>
2023-08-15 09:53:20 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign _ Entry point
+ *
+ * @n: is the number to check
+ * Return: 1 if n is greater than 0, if n is 0 return 0 otherwise return -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+
+		return (-1);
+	}
+}
