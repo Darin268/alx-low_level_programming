e780cfd19adfe96889e7eaf7aec5700956fd330c
“Dino” <daringamaleldin@hotmail.com>
2023-08-15 12:18:51 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ *
+ * @n: is the number to check
+ *
+ * Return: 0
+ */
+int print_last_digit(int n)
+{
+	int y, x
+
+		if (n < 0)
+		{
+			y = n * -1;
+			x = y % 10;
+			return (x);
+		}
+		else
+		{
+			x = n % 10;
+			return (x);
+		}
+}
