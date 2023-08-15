899a1615b6e4d587704e368ee11e7c4589b09d23
“Dino” <daringamaleldin@hotmail.com>
2023-08-15 14:53:10 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * @a: is the number to add and check
+ * @b: is the number to add and check
+ *
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	int z;
+
+	for (a > 0 && b > 0)
+{
+	z = a + b;
+	_putchar(z);
+	return (z);
+}
+}
