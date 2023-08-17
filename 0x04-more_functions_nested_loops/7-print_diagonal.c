297e97f7f4a989d5f58a87c129b28990f50a25e8
“Dino” <daringamaleldin@hotmail.com>
2023-08-17 12:16:35 -0700

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,12 +15,18 @@ void print_diagonal(int n)
 		}
 		else
 		{
-			int i;
+			int i, j;
 
 			for (i = 1; i < n; i++)
 			{
+				for (j = 0; j < n; j++)
+				{
+					if (j == i)
 				_putchar('\\');
-			}
+					else if (j < i)
+						_putchar(' ');
+				}
 			_putchar('\n');
+			}
 		}
 }
