999fb66e64f90546cbd5eaee65e11ebf9ba6d707
“Dino” <daringamaleldin@hotmail.com>
2023-08-17 13:37:33 -0700

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,34 +2,30 @@
 
 /**
  * print_square - to print square
- * @size: is the size of the square
+ * @size: is the size of square
  * Return: Always 0 (Success)
  */
 
 void print_square(int size)
 {
-	if (size <= 0)
-	{
 
-		_putchar('\n');
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i, j;
 
-	}
-	else
-	{
-		int i, j;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
 
-		for (i = 0; i < size; i++)
-		{
+_putchar('#');
 
-			for (j = 0; j < size; j++)
-
-			{
-
-		_putchar('#');
-
-			}
-
-			_putchar('\n');
-		}
-	}
+}
+_putchar('\n');
+}
+}
 }
