07a606a768ddcb799ed8c2f42bcc89ca6f2288d0
“Dino” <daringamaleldin@hotmail.com>
2023-08-17 12:01:43 -0700

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - to print diagonal
+ * @n: number of diagonal to print
+ * Return: Always 0 (Success)
+ */
+
+void print_diagonal(int n)
+{
+
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			int i;
+
+			for (i = 1; i <= n; i++)
+			{
+				_putchar('\');
+			}
+			_putchar('\n');
+		}
+}
