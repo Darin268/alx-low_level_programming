6a360b1ee2c23b6704ae45fec60e7c4e7d86a82b
“Dino” <daringamaleldin@hotmail.com>
2023-08-17 07:53:38 -0700

    print most number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *
+ * print_most_numbers
+ *
+ * Return: 0 Always (Success)
+ */
+
+void print_most_numbers(void)
+{
+	int y;
+
+	for (y = 0; y <= 9; y++)
+	{
+		if (y != 2 && y != 4)
+		{
+		_putchar(y + '0');
+		}
+	}
+	_putchar('\n')
+}
