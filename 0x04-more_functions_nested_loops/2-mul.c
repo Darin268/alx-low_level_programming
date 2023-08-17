513df271968b29e8c65fb9e99fc4f20c62e6e155
“Dino” <daringamaleldin@hotmail.com>
2023-08-17 07:02:12 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ * mul - Entry point
+ *
+ * @a: first character to multiply
+ * @b: second character to multiply
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	int x;
+
+	x = a * b;
+
+	_putchar(x);
+	return (0);
+}
