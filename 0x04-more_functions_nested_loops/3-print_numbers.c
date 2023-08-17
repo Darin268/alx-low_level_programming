7c4365f57ee0e347769afecb712d45e9a2538429
“Dino” <daringamaleldin@hotmail.com>
2023-08-17 07:26:16 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - To print numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a >= 0; a <= 9; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n')
+}
