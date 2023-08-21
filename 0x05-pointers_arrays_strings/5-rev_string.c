62ea240aa638cc7a7004552ba049febb520102dd
“Dino” <daringamaleldin@hotmail.com>
2023-08-21 16:45:36 -0700

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - Entry point
+ * @s: string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int i, rev, rev1
+	char j;
+	rev1 = 0;
+
+	for (rev = 0; s[rev] != '\0'; ++rev)
+	{
+	}
+
+	rev1 = rev - 1;
+
+	for (i = 0; i < rev / 2; i++)
+	{
+		j = s[i];
+		s[i] = s[rev1];
+		s[rev1--] = c;
+	}
+}
