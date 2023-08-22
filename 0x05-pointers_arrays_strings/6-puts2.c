f9c4f8d361e5acf9176a8dec24cbb277afbe029a
“Dino” <daringamaleldin@hotmail.com>
2023-08-21 17:02:21 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * @str: string to check
+ */
+
+void puts2(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[j]);
+	}
+
+	_putchar('\n');
+}
