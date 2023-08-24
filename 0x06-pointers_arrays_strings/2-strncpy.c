def7c9cb7d33dc564807f54ec0b9714dffa6155b
“Dino” <daringamaleldin@hotmail.com>
2023-08-24 04:03:52 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - Entry point
+ * @dest: is the first string
+ * @src: is the second string
+ * @n: is the number of bytes
+ *
+ * Return: to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+
+	while (dest[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
