d1077c15fce05eaad80bd081ca8efedf2e4a2e3e
“Dino” <daringamaleldin@hotmail.com>
2023-08-24 05:11:32 -0700

    rev array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - Entry point
+ * @a: is the string to check
+ * @n: number of elements of the array
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, j, tmp;
+
+	j = n - 1;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+		j--;
+	}
+}
