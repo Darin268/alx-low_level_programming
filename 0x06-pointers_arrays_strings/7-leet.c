1bd92a6189dc93528a6ae097d685ae619bb2449e
“Dino” <daringamaleldin@hotmail.com>
2023-08-24 06:54:18 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - Entry point
+ * @c: is the string to check
+ *
+ * Return: to c
+ */
+
+char *leet(char *c)
+{
+	int i, j;
+
+	char a[10] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char b[10] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (c[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+	return (c);
+}
