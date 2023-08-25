bdeeb336d4bf0e1246543781688436e5451e3e33
“Dino” <daringamaleldin@hotmail.com>
2023-08-25 06:38:14 -0700

    print number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - Entry point
+ * @n: is the integer to print
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+
+	else if (n == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		_putchar(n);
+	}
+if (n / 10)
+
+	print_number(n / 10);
+	_putchar((n % 10) + '0');
+}
