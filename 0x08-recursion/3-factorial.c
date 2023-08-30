175ed15e247071181fb98bf3ea7167ed601ac608
“Dino” <daringamaleldin@hotmail.com>
2023-08-30 16:56:20 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - Entry point
+ * @n: is the number to check
+ * Return: to the factorial of n
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
