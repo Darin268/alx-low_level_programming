95b821037a5bee4a5f9fc769ec3503bbefd384d2
“Dino” <daringamaleldin@hotmail.com>
2023-08-31 11:14:53 -0700

    sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -28,9 +28,7 @@ int _sqrt_recursion(int n)
 int actual_sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
-	{
 		return (-1);
-	}
 	if (i * i == 0)
 		return (i);
 	return (actual_sqrt_recursion(n, i + 1));
