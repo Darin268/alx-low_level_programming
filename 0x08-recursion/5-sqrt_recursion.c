193914bd4644ec0c86a60a53bb25fa3eabda4d38
“Dino” <daringamaleldin@hotmail.com>
2023-08-31 10:44:40 -0700

    sqrt recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
-int actual_sqrt_recursion(int i, int n);
+int actual_sqrt_recursion(int n, int i);
 
 /**
  * _sqrt_recursion - Entry point
  * actual_sqrt_recursion - to find natural 
  * square root of a number
- * @i: iterator
  * @n: is the int to check
  * Return: to natural square root of n
  */
@@ -14,9 +13,9 @@ int actual_sqrt_recursion(int i, int n);
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-
+	{
 		return (-1);
-
+	}
 
 		return (actual_sqrt_recursion(n, 0));
 }
