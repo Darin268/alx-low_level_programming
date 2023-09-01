00abe71a91ee74519b49ddd7cb7df73f45fe4cc4
dino <daringamaleldin@gmail.com>
2023-09-01 15:08:25 +0000

    add two numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * main - to add two positive numbers
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 1 if Error and 0 if nothing is passed
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int n = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < 48 || argv[i][j] > 57)
+			{
+				printf("Error\n")
+					return (1);
+			}
+		}
+
+		n = n + atoi(argv[i]);
+	}
+	printf("%d\n", n);
+	return (0);
+}
