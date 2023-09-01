f59c6a72889c5d2ba4e3a4ecf4e7bd562ca7c5a3
dino <daringamaleldin@gmail.com>
2023-09-01 14:20:44 +0000

    print all args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - to print all args content
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int x;
+
+	for (x = 0; x < argc; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
+
+	return (0);
+}
