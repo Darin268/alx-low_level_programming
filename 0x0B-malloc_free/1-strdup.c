95450e13a81a422626b17a2685d7964a6484718a
“Dino” <daringamaleldin@hotmail.com>
2023-09-04 12:01:14 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Entry point
+ * @str: is the string to duplicate
+ *
+ * Return: to the duplicate string and NULL if str is NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *c;
+	unsigned int a, b;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+	}
+
+	c = malloc(sizeof(char) * a + 1);
+
+	if (c == NULL)
+	{
+		return (NULL);
+	}
+
+	for (b = 0; str[b] != '\0'; b++)
+	{
+	}
+	return (c);
+}
