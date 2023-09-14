d3f57d29e71db03a0b4a84fe087623d7fcd3620d
“Dino” <daringamaleldin@hotmail.com>
2023-09-14 11:37:46 -0700

    sum them all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
 #ifndef MYHEADERS_H
 #define MYHEADERS_H
+#include <stdrag.h>
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
