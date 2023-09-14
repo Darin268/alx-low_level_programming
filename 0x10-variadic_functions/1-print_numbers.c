497bcd878618874f8ec891765d7cffd3e232fce1
“Dino” <daringamaleldin@hotmail.com>
2023-09-14 12:28:49 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,11 +14,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list list;
 
 
-va_start(list, a);
+va_start(list, n);
 
 for (a = 0; a < n; a++)
 {
-printf("%d", va_arg(list, a);
+printf("%d", va_arg(list, int));
 
 if (a != n - 1 && separator != NULL)
 printf("%s", separator);
