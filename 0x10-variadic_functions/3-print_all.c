eeddbcb93c18c373673ea22a47a732b396740b2c
“Dino” <daringamaleldin@hotmail.com>
2023-09-15 07:13:20 -0700

    print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,96 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_all - to print anything
+ * @format: list of types of arguments to print
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list list;
+
+	f_id form_types[] = {
+		{ "c", print_a_char },
+		{ "i", print_an_integer },
+		{ "f", print_a_float },
+		{ "s", print_a_char_ptr }
+	};
+
+	unsigned int a = 0;
+	unsigned int b = 0;
+	char *separator;
+
+	va_start(list, format);
+
+	while (format != NULL && format[a])
+	{
+		b = 0;
+		while (b < 4)
+		{
+			if (format[a] == *form_types[b].id)
+			{
+				form_types[b].f(separator, list);
+				separator = ", ";
+			}
+			b++;
+		}
+		a++;
+	}
+
+	va_end(list);
+	printf("\n");
+}
+
+/**
+ * print_a_char - to print a char type
+ * @separator: is the separator between char
+ * @list: is the list of arguments
+ */
+
+void print_a_char(char *separator, va_list list)
+{
+printf("%s%c", separator, va_arg(list, int));
+}
+
+/**
+ * print_an_integer - to print an integer type
+ * @separator: is the separator between integers
+ * @list: is the list of arguments
+ */
+
+void print_an_integer(char *separator, va_list list)
+{
+printf("%s%i", separator, va_arg(list, int));
+}
+
+/**
+ * print_a_float - to print a float type
+ * @separator: is the separator between float
+ * @list: is the list of arguments
+ */
+
+void print_a_float(char *separator, va_list list)
+{
+	printf("%s%f", separator, va_arg(list, double));
+}
+
+/**
+ * print_a_char_ptr - to print a pointer to char type
+ * @separator: is the separator between char
+ * @list: is the list of arguments
+ */
+
+void print_a_char_ptr(char *separator, va_list list)
+{
+	char *c = va_arg(list, char *);
+
+	if (c == NULL)
+	{
+		printf("%s%s", separator, "(nil)");
+		return;
+	}
+
+	printf("%s%s", separator, c);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,4 +8,16 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct format_list - struct format_list
+ * @id: the format
+ * @f: function pointer
+ */
+typedef struct format_list
+{
+	char *id;
+	void (*f)(char *separator, va_list list);
+}
+f_id;
+
 #endif
