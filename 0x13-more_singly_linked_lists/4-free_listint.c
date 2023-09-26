e291fcb2f09ee89464e2692605f6414aed95994c
“Dino” <daringamaleldin@hotmail.com>
2023-09-26 12:19:47 -0700

    free listint

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - to free a list
+ * @head: is the head of the list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *list;
+
+	while (head)
+	{
+		list = head;
+		head = (head).next;
+		free(list);
+	}
+}
