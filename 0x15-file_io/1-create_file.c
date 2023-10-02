3675499e47e54af0455ade797e82fbc63e264db2
“Dino” <daringamaleldin@hotmail.com>
2023-10-02 15:47:09 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,6 +15,14 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+	{
+		o = open(filename, O_WRONLY | O_CREAT, 0600);
+		if (o == -1)
+			return (-1);
+		return (1);
+	}
+
 	if (text_content != NULL)
 	{
 		for (a = 0; text_content[a]; a++)
