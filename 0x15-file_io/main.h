b2938b0ae3b11f463db2af4cc115ba5de901e462
“Dino” <daringamaleldin@hotmail.com>
2023-10-02 14:44:45 -0700

    read text file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,9 @@
 #define MYHEADERS_H
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
