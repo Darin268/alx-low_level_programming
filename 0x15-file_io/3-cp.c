6877f61b0b46c58a419837570f058d97f332668b
“Dino” <daringamaleldin@hotmail.com>
2023-10-04 06:54:20 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,33 +21,33 @@ int main(int argc, char **argv)
 	}
 	_from = open(argv[1], O_RDONLY);
 	if (_from == -1)
-	{	dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", argv[1]);
+	{	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (_to == -1)
-	{	dprintf(STDERR_FILENO, "Error: Can't write to%s\n", argv[2]);
+	{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	while ((rd = read(_from, buff, BUFSIZ)) > 0)
 	{
 		if (write(_to, buff, rd) != rd)
-			dprintf(STDERR_FILENO, "Error: Can't write to%s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	if (rd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (close(_from) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", _from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", _from);
 		exit(100);
 	}
 	if (close(_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd%d\n", _to);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", _to);
 		exit(100);
 	}
 	return (0);
