517241ce4dfec55bb7362307dd14658144cee255
“Dino” <daringamaleldin@hotmail.com>
2023-10-04 06:05:25 -0700

    test

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,9 +10,19 @@
 
 int main(int argc, **argv)
 {
+	int _from = 0, _to = 0;
+	char buff[BUFSIZ];
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
+	}
+
+	_from = open(av[1], O_RDONLY);
+	if (_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", argv[1]);
+		exit(98);
 	}
 }
