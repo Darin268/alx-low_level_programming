787034fd24a7dd4a8d137b1b50944cddba7f34ea
“Dino” <daringamaleldin@hotmail.com>
2023-10-26 03:52:18 -0700

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * flip_bits - flip to get from one number to another
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits that was needed to flip
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int x;
+	int count;
+
+	x = n ^ m;
+	count = 0;
+
+	while (x != NULL)
+	{
+		if (x & 1)
+			count++;
+
+		x >>= 1;
+	}
+
+	return (count);
+}
