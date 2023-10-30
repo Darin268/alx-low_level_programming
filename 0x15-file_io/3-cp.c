40f4d294003fc576a4a303d510509cb24ca0fc68
“Dino” <daringamaleldin@hotmail.com>
2023-10-30 03:48:54 -0700

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,17 +1,14 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "holberton.h"
 /**
- * main - Entry point
- * @argc: arguments count
- * @argv: list of arguments
- * Return: 0 for success and exit codes for failure
+ * main - copies the content of one file to another
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 if success
  */
-
 int main(int argc, char *argv[])
 {
-	int _from, _to, rd;
+	int ffrom, fto, rd, clf, clt;
 	char buff[BUFSIZ];
 
 	if (argc != 3)
@@ -19,31 +16,32 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	_from = open(argv[1], O_RDONLY);
-	if (_from == -1)
-	{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	ffrom = open(argv[1], O_RDONLY);
+	if (ffrom  == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	while ((rd = read(_from, buff, BUFSIZ)) > 0)
-	{
-		if (_to == -1 || (write(_to, buff, rd) != rd))
+	fto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	while ((rd = read(ffrom, buff, BUFSIZ)) > 0)
+		if (fto == -1 || (write(fto, buff, rd) != rd))
+		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+			exit(99);
+		}
 	if (rd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(_from) == -1)
+	clf = close(ffrom);
+	clt = close(fto);
+	if (clf == -1 || clt == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", _from);
-		exit(100);
-	}
-	if (close(_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", _to);
+		if (clf == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ffrom);
+		else if (clt == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fto);
 		exit(100);
 	}
 	return (0);
