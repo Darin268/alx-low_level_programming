1e8bae06ac822b0e87dc3ff0ef8e4c35204814ac
“Dino” <daringamaleldin@hotmail.com>
2023-10-30 02:25:23 -0700

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		o = open(filename, 0_WRONLY | 0_CREAT, 0600);
+		o = open(filename, O_WRONLY | O_CREAT, 0600);
 		if (o == -1)
 			return (-1);
 		return (1);
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 		{
 		}
 	}
-	o = open(filename, 0_CREAT | 0_RDWR | 0_TRUNC, 0600);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (o == -1)
 		return (-1);
 
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,13 +19,13 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	_from = open(argv[1], 0_RDONLY);
+	_from = open(argv[1], O_RDONLY);
 	if (_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	_to = open(argv[2], 0_WRONLY | 0_CREAT | 0_TRUNC, 0664);
+	_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Cant't write to %s\n", argv[2]);
