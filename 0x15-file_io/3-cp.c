d090d81cf768ecbf9a05c03aaf14249d4bd46ef8
“Dino” <daringamaleldin@hotmail.com>
2023-10-30 03:56:43 -0700

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int ffrom, fto, rd, clf, clt;
+	int _from, _to, rd;
 	char buff[BUFSIZ];
 
 	if (argc != 3)
@@ -18,15 +18,15 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	ffrom = open(argv[1], O_RDONLY);
-	if (ffrom  == -1)
+	_from = open(argv[1], O_RDONLY);
+	if (_from  == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fto = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	while ((rd = read(ffrom, buff, BUFSIZ)) > 0)
-		if (fto == -1 || (write(fto, buff, rd) != rd))
+	_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	while ((rd = read(_from, buff, BUFSIZ)) > 0)
+		if (_to == -1 || (write(_to, buff, rd) != rd))
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
@@ -36,15 +36,16 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	clf = close(ffrom);
-	clt = close(fto);
-	if (clf == -1 || clt == -1)
+	if (close(_from) == -1)
 	{
-		if (clf == -1)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ffrom);
-		else if (clt == -1)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fto);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", _from);
 		exit(100);
 	}
+	if (close(_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", _to);
+		exit(100);
+	}
+
 	return (0);
 }
