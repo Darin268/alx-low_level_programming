8e41ec74a16eb6063ccf3ddc7d10d5033338b243
“Dino” <daringamaleldin@hotmail.com>
2023-10-30 03:01:01 -0700

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,14 +21,12 @@ int main(int argc, char *argv[])
 	}
 	_from = open(argv[1], O_RDONLY);
 	if (_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	{ dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	{ dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	while ((rd = read(_from, buff, BUFSIZ)) > 0)
