e8c0d940c0c29ccbbf510f501b295fba0eebc761
“Dino” <daringamaleldin@hotmail.com>
2023-12-07 13:30:45 -0800

    delete dnodeint

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -4,7 +4,7 @@
  * delete_dnodeint_at_index - to delete node at given index
  * @head: is the beginning of the list
  * @index: index to delete
- * Returns: 1 if it succeeded, -1 if it failed
+ * Return: 1 if it succeeded, -1 if it failed
  */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
