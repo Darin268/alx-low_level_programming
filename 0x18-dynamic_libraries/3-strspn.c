1ca2924375edb02dc173ccd1fa124cbf4af6e772
“Dino” <daringamaleldin@hotmail.com>
2023-12-11 15:44:08 -0800

    update

---
+++ 0x18-dynamic_libraries/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strspn - Entry point
+ * @s: is the string
+ * @accept: the bytes to count
+ *
+ * Return: the number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+	int c = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] != 32)
+		{
+			for (j = 0; accept[j] != '\0'; j++)
+			{
+				if (s[i] == accept[j])
+					c++;
+			}
+		}
+		else
+		{
+			return (c);
+		}
+	}
+	return (c);
+}
